4ab6789f45be38e6366af0431102014897bb4cd3
vaasu <mohiit1502@gmail.com>
2019-11-28 01:07:00 +0530

    Initial build subtree commit

---
+++ build/precache-manifest.51ca93c08c29b2afbc20bfa75fd61650.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d23304c5bfc219dad83cabdb2db7ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ecf652483e9c4b4fcc",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c130c72a2d8aba565637",
+    "url": "/static/css/3.fbef6aef.chunk.css"
+  },
+  {
+    "revision": "cb01553353cd3143bd68",
+    "url": "/static/css/main.ca2433bd.chunk.css"
+  },
+  {
+    "revision": "80ecf652483e9c4b4fcc",
+    "url": "/static/js/2.00e3390c.chunk.js"
+  },
+  {
+    "revision": "c130c72a2d8aba565637",
+    "url": "/static/js/3.96c80365.chunk.js"
+  },
+  {
+    "revision": "96e6d4e29fb54bbf1652",
+    "url": "/static/js/4.ca966415.chunk.js"
+  },
+  {
+    "revision": "5282582bccab2ab5c2fe",
+    "url": "/static/js/5.7c430a2a.chunk.js"
+  },
+  {
+    "revision": "cb01553353cd3143bd68",
+    "url": "/static/js/main.1d85f159.chunk.js"
+  },
+  {
+    "revision": "c436c7f8cdf1df90f0e8",
+    "url": "/static/js/runtime-main.5cf2396a.js"
+  }
+]);
