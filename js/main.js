9ed9526ebafc1f77874acf852578e35d8b74581e
vaasu <mohiit1502@gmail.com>
2019-12-24 02:02:20 +0530

    Fixed marquee bug and added services' HTMLs

--- js/main.js
+++ js/main.js
@@ -24,10 +24,11 @@ $(document).on('ready', function () {
 	const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 	const marqueeContent = document.querySelector("ul.marquee-content");
 
-	root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-
-	for(let i=0; i<marqueeElementsDisplayed; i++) {
-		marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+	if (marqueeContent) {
+		root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+		for(let i=0; i<marqueeElementsDisplayed; i++) {
+			marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+		}
 	}
 
 	while (length--) {
