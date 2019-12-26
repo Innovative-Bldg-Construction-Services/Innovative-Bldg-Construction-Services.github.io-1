0d709a2d7e1e0f346222d34a53f44e2e7e5efdf8
vaasu <mohiit1502@gmail.com>
2019-12-26 23:39:01 +0530

    Test: Download service details

--- js/main.js
+++ js/main.js
@@ -446,5 +446,57 @@ $(document).on('ready', function () {
 		// shadowBall.posy.value = e.pageY;
 	});
 
+	$('#button-download').on('click', function convert_HTML_to_PDF() {
+        var pdf = new jsPDF('p', 'pt', 'letter');
+        // source can be HTML-formatted string, or a reference
+        // to an actual DOM element from which the text will be scraped.
+		var source = $('#menu')[0];
+		
+		pdf.setTextColor(0, 0, 0)
+
+        // we support special element handlers. Register them with jQuery-style 
+        // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+        // There is no support for any other type of selectors 
+        // (class, of compound) at this time.
+        var specialElementHandlers = {
+            // element with id of "bypass" - jQuery style selector
+            '#page-loader': function (element, renderer) {
+                // true = "handled elsewhere, bypass text extraction"
+                return true
+			},
+            '#navbarMenu': function (element, renderer) {
+                // true = "handled elsewhere, bypass text extraction"
+                return true
+			},
+            '#article-slider': function (element, renderer) {
+                // true = "handled elsewhere, bypass text extraction"
+                return true
+			}
+        };
+        var margins = {
+            top: 80,
+            bottom: 60,
+			left: 40,
+			right: 40,
+            width: 522
+        };
+        // all coords and widths are in jsPDF instance's declared units
+        // 'inches' in this case
+        pdf.fromHTML(
+            source, // HTML string or DOM elem ref.
+            margins.left, // x coord
+            margins.top, { // y coord
+                'width': margins.width, // max width of content on PDF
+                'elementHandlers': specialElementHandlers
+            },
+
+            function (dispose) {
+                // dispose: object with X, Y of the last line add to the PDF 
+                //          this allow the insertion of new lines after html
+                pdf.save('Test.pdf');
+            }, margins
+        );
+    })
+
 });
 
